971d5b84759ca9a2930b354a1d9a32913c9433d7
Elisée Bangya <bangyanelson@gmail.com>
2021-03-01 19:45:23 +0100

    My first commited programm in C

---
+++ main.c
@@ -0,0 +1,44 @@
+/*
+
+Plus ou Moins
+-------------
+Réalisé par Elisée Bangya (cours de programmation en C)
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main ( int argc, char** argv )
+{
+    int nombreMystere = 0, nombreEntre = 0;
+    const int MAX = 100, MIN = 1;
+
+    // GÈnÈration du nombre alÈatoire
+
+    srand(time(NULL));
+    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+
+    /* La boucle du programme. Elle se rÈpËte tant que l'utilisateur
+    n'a pas trouvÈ le nombre mystËre */
+
+    do
+    {
+        // On demande le nombre
+        printf("Quel est le nombre ? ");
+        scanf("%d", &nombreEntre);
+
+        // On compare le nombre entrÈ avec le nombre mystËre
+
+        if (nombreMystere > nombreEntre)
+            printf("C'est plus !\n\n");
+        else if (nombreMystere < nombreEntre)
+            printf("C'est moins !\n\n");
+        else
+            printf ("Bravo, vous avez trouve le nombre mystere !!!\n\n");
+    } while (nombreEntre != nombreMystere);
+
+}
